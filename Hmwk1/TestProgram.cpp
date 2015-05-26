cfe8dcbafe5224bc59b43af98a126ecdab168f40
Rhernandez513 <rhernandez513@gmail.com>
2015-05-26 11:19:25 -0500

    cube modified to test for bad values

--- Hmwk1/TestProgram.cpp
+++ Hmwk1/TestProgram.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main()
 {
   int x;
-  Cube cubetest(5, 5, 5);
-  cout << "The area is: " << cubetest.get_area() << endl;
-  cout << "The length is: " << cubetest.get_length() << endl;
-  cout << "The depth is: " << cubetest.get_depth() << endl;
-  cout << "The width is: " << cubetest.get_width() << endl;
-  cout << "The surface area is: " << cubetest.get_surface_area() << endl;
+  Cube cubeTest(5, 5, 5);       // Creates a correctly working Cube
+
+  //Uncomment line to check negative values
+  // Cube failCubebTest(5, -5, 5); // Should fail and throw an exception
+
+  cout << "The area is: " << cubeTest.get_area() << endl;
+  cout << "The length is: " << cubeTest.get_length() << endl;
+  cout << "The depth is: " << cubeTest.get_depth() << endl;
+  cout << "The width is: " << cubeTest.get_width() << endl;
+  cout << "The surface area is: " << cubeTest.get_surface_area() << endl;
   cin >> x;
   return 0;
 }
--- Hmwk1/cube.cpp
+++ Hmwk1/cube.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <string>
+#include <array>
 #include "cube.h"
+
 
 Cube::Cube(int inLength, int inWidth, int inDepth)
   : length(inLength),      // Is it really a cube?
   width(inWidth),          // With potentially 
   depth(inDepth)           // Different sides?
 {
+  using namespace std;
+  array <int, 3> values = { length, width, depth };
+  string name[] = { "length", "width", "depth" };
+  for (int dimension = 0; dimension < values.size() ; dimension++)
+  {
+    if (values[dimension] < 0)
+    {
+      string message = "The " + name[dimension] + " is " + 
+                       to_string(values[dimension]) +  " less than zero.";
+      cout << message << endl;
+      throw invalid_argument(message);
+    }
+  }
 }
 
 // return length of cube
