6ce5082eaea15d4ac04f00ef97a7c741b0f16ef0
Rhernandez513 <rhernandez513@gmail.com>
2015-05-27 20:03:01 -0500

    added filters to examples and lab2

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -11,44 +11,45 @@ bool set_ships(int **& matrix, int size, int num1);
 
 int main()
 {
-	int ** B;
-	int size, num_ships, row, col;
-	int num_hits=0;
-    
-	cout << "***** Battleship *****" << endl;
-	cout << "How many rows/columns is the board? ";
-	cin >> size;
-    
-    /* CREATE THE BATTLESHIP BOARD AND SET ALL ELEMENTS TO 0*/
+  int ** B;
+  int size, num_ships, row, col;
+  int num_hits = 0;
+
+  cout << "***** Battleship *****" << endl;
+  cout << "How many rows/columns is the board? ";
+  cin >> size;
+
+  /* CREATE THE BATTLESHIP BOARD AND SET ALL ELEMENTS TO 0*/
 
 
-    
-	cout << "A " << size << "x" << size << " board has been created.\n";
-	cout << "How many ships? ";
-	cin >> num_ships;
-	set_ships(B,size,num_ships);
-    
-	while(num_hits!=num_ships)
-	{
-		cout << "What row:";
-		cin >> row;
-		cout << "What col: ";
-		cin >> col;
-        
-        /*DETERMINE IF ITâS A HIT. IF SO, UPDATE INFORMATION & TELL USER.*/
 
-	}
-    
-	cout << "You sunk my battleships!" << endl;
-    
-    /*CLEAN UP YOUR MEMORY*/
-  
+  cout << "A " << size << "x" << size << " board has been created.\n";
+  cout << "How many ships? ";
+  cin >> num_ships;
+  set_ships(B, size, num_ships);
 
-    
-	return 0;
+  while (num_hits != num_ships)
+  {
+    cout << "What row:";
+    cin >> row;
+    cout << "What col: ";
+    cin >> col;
+
+    /*DETERMINE IF ITâS A HIT. IF SO, UPDATE INFORMATION & TELL USER.*/
+
+  }
+
+  cout << "You sunk my battleships!" << endl;
+
+  /*CLEAN UP YOUR MEMORY*/
+
+
+
+  return 0;
 }
 /** Create set_ships function **/
-bool set_ships(int **& matrix, int size, int num1){
-    return true;
+bool set_ships(int **& matrix, int size, int num1)
+{
+  return true;
 }
 
