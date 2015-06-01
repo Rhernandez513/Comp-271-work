b39eb0a0ca8d31885f1c2bc32f256f8b0e7ab089
Rhernandez513 <rhernandez513@gmail.com>
2015-05-31 23:04:07 -0500

    semi-finished set-ships func

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -3,6 +3,7 @@
  *
  */
 #include <iostream>
+#include <time.h>
 #include <cstdlib>
 
 using namespace std;
@@ -20,14 +21,28 @@ int main()
   cin >> size;
 
   /* CREATE THE BATTLESHIP BOARD AND SET ALL ELEMENTS TO 0*/
-
+  // Creating the Board
+  B = new int * [size];
+  for(int i = 0; i < size; i++)
+  {
+    B[i] = new int[size];
+  }
+  // Initializing Elements to 0
+  for(int row = 0; row < size; row++)
+  {
+    for(int column = 0; column < size; column++)
+    {
+      B[row][column] = 0;
+    }
+  }
 
 
   cout << "A " << size << "x" << size << " board has been created.\n";
   cout << "How many ships? ";
   cin >> num_ships;
   set_ships(B, size, num_ships);
-
+  cout << "paused" << endl;
+  system("pause");
   while (num_hits != num_ships)
   {
     cout << "What row:";
@@ -50,6 +65,22 @@ int main()
 /** Create set_ships function **/
 bool set_ships(int **& matrix, int size, int num1)
 {
+  srand((unsigned)time(NULL));
+  for(int i = 0; i < num1; i++)
+  {
+    int row = (rand() % size);
+    int column = (rand() % size);
+    int* location = &matrix[row][column];
+    if (*location == 1)
+    {
+      i--;
+      continue;
+    }
+    else
+    {
+      *location = 1;
+      cout << "set ship at " << location << endl;
+    }
+  }
   return true;
 }
-
