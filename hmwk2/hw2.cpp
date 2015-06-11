171b374d5872323eaa75665945df85ea79897b65
Robert Hernandez <rhernandez513@gmail.com>
2015-06-11 18:05:03 -0500

    hmwk2 code portion complete

--- hmwk2/hw2.cpp
+++ hmwk2/hw2.cpp
@@ -1,90 +1,130 @@
 #include <iostream>
 #include <cstring>
-#include "hw2.h"
+#include "ALBUM.h"
 
-using namespace std;
+using namespace hw2;
+  Album::Album()
+  {
+    artist = new char[256];
+    title = new char[256];
+    year_released = 0;
+    record_label = new char[256];
+    num_songs = 0;
+    num_minutes_long = 0;
+    genre = new char[256];
+  };
 
-Album::Album()
-{
-  artist = new char[256];
-  title = new char[256];
-  year_released = 0;
-  record_label = new char[256];
-  num_songs = 0;
-  num_minutes_long = 0;
-  genre = new char[256];
-}
+  Album::Album(char *& a, char *& t)
+    : artist(a)
+    , title(t)
+  {
+  };
 
-Album::Album(char *& a, char *& t)
-  : Album()
-{
-  artist = a;
-  title = t;
-}
+  //deconstructor
+  Album::~Album() { } ;
 
-//deconstructor
-Album::~Album()
-{
-  delete[] artist;
-  delete[] title;
-  delete[] record_label;
-  delete[] genre;
-}
+  // Setters
+  bool Album::set_artist(char * a) 
+  { 
+    if (a) {
+      artist = a; 
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_title(char * t) 
+  { 
+    if (t) {
+      title = t;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_year_released(int value)
+  {
+    if (value > 0) {
+      year_released = value;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_record_label(char *& label) 
+  {
+    if (label) {
+      record_label = label; 
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_num_songs(int value)
+  {
+    if (value > 0) {
+      num_songs = value;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_num_minutes_long(int value)
+  {
+    if (value > 0) {
+      num_minutes_long = value;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool Album::set_genre(char * g)
+  {
+    if (g) {
+      genre = g; 
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-// Setters
-bool Album::set_artist(char * a) { artist = a; }
-bool Album::set_title(char * t) { title = t; }
-bool Album::set_year_released(int value) { year_released = value; }
-bool Album::set_record_label(char *& label) { record_label = label; }
-bool Album::set_num_songs(int value) { num_songs = value; }
-bool Album::set_num_minutes_long(int value) { num_minutes_long = value; }
-bool Album::set_genre(char * g) { genre = g; }
-
-// Getters
-int Album::get_num_songs() { return num_songs; }
-int Album::get_num_minutes_long() { return num_minutes_long; }
-int Album::get_year_released() { return year_released; }
-bool Album::get_artist(char *& a)
-{
-  if (artist)
+  // Getters
+  int Album::get_num_songs() { return num_songs; }
+  int Album::get_num_minutes_long() { return num_minutes_long; }
+  int Album::get_year_released() { return year_released; }
+  bool Album::get_artist(char *& a)
   {
-    a = artist;
-    return true;
+    if (artist) {
+      a = artist;
+      return true;
+    } else {
+      return false;
+    }
   }
-  else
+  bool Album::get_title(char *& t)
   {
-    return false;
+    if (title) { 
+      t = title;
+      return true;
+    } else {
+      return false;
+    }
   }
-}
-bool Album::get_title(char *& t)
-{
-  if (title)
+  bool Album::get_record_label(char *& label)
   {
-    t = title;
-    return true;
+    if (record_label) {
+      label = record_label;
+      return true;
+    } else {
+      return false;
+    }
   }
-  else
+  bool Album::get_genre(char *& g)
   {
-    return false;
+    if (genre) {
+      g = genre;
+      return true;
+    } else {
+      return false;
+    }
   }
-}
-bool Album::get_record_label(char *& label)
-{
-  if (record_label)
-  {
-    label = record_label;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-bool Album::get_genre(char *& g)
-{
-  if (genre)
-  {
-    g = genre;
-  }
-  return genre;
-};
--- hmwk2/hw2main.cpp
+++ hmwk2/hw2main.cpp
@@ -1,58 +1,66 @@
 #include <iostream>
 #include <cstring>
-#include "hw2.h"
+#include "ALBUM.h"
+using namespace hw2;
+Album ** CollectionCreator ();
+  const int collectionSize = 3;
 
-using namespace std;
+  void main () 
+  {
+    // Create holders for printing variable
+    char ** outTitle = new char * [collectionSize];
+    char ** outArtist = new char * [collectionSize];
+    *outTitle = new char [256];
+    *outArtist = new char [256];
 
-Album* CollectionCreator ();
-const int collectionSize = 3;
+    // Instantiate my music collection
+    Album ** myCollection = CollectionCreator();
 
-void main () 
-{
-  // Create holders for printing variable
-  char ** outTitle;
-  char ** outArtist;
-  char * outTitle = new char [collectionSize];
-  char * outArtist = new char [collectionSize];
-  
-  // Instantiate my music collection
-  Album* myCollection = CollectionCreator();
-
-  // Simple loop to print contents of the albumCollection
-  for(int i = 0; i < collectionSize; i++) {
-    if ((myCollection[i].get_title(outTitle[i]))){
-      cout << "Album Title #" << i << outTitle[i] << endl;
+    // Simple loop to print contents of the albumCollection
+    for(int i = 0; i < collectionSize; i++) {
+      if ((myCollection[i]->get_title(outTitle[i]))){
+        std::cout << "Album Title #" << i + 1 << " " << outTitle[i] << std::endl;
+      }
+      if (myCollection[i]->get_artist(outArtist[i])){
+        std::cout << "By: " << outArtist[i] << "\n" << std::endl;
+      }
     }
-    if (myCollection[i].get_artist(outArtist[i])){
-      cout << "By: " << outArtist[i] << "\n" << endl;
-    }
-  }
-  
-  // Memory Cleanup
-  delete [] outTitle;
-  delete [] outArtist;
-  for (int i = 0; i < collectionSize; i++) {
-   delete &myCollection[i];
-  }
-  delete [] myCollection;
-}
 
-// Creates collection of albums, allows the main
-// to stand alone without hard coded variables
-Album* CollectionCreator ()
-{
-  Album ** albumCollection;
-  char ** artist;
-  char * inArtists = { "Monstercat" } ;
-  char * inTitles[256] = { "Solace", "Aspect", "Contact" } ;
-  char * artist = new char [collectionSize] ;
-  for (int i = 0; i < collectionSize; i++) {
-    artist[i] = &inArtists[0];
+    // Memory Cleanup
+    delete [] outTitle;
+    delete [] outArtist;
+    for (int i = 0; i < collectionSize; i++) {
+      delete *&myCollection[i];
+    }
+    delete [] myCollection;
+    std::cout << "Horray!" << std::endl;
+    std::system("pause");
   }
-  for (int i = 0; i < collectionSize; i++) {
-    albumCollection[i] = new Album(*&inTitles[i], *&artist[i]);
+
+  // Creates collection of albums, allows the main
+  // to stand alone without hard coded variables
+  Album** CollectionCreator ()
+  {
+    Album ** albumCollection = new Album * [collectionSize];
+    char ** artist = new char * [collectionSize];
+    for(int i = 0; i < collectionSize; i++) {
+      artist[i] = new char [256];
+    }
+
+    // Album titles
+    char * inTitles[256] = { "Solace", "Aspect", "Contact" } ;
+
+    // Create Matching array of artist (same artist for all 3 albums)
+    char * inArtists = { "Monstercat" } ;
+    for (int i = 0; i < collectionSize; i++) {
+      artist[i] = &inArtists[0];
+    }
+    for (int i = 0; i < collectionSize; i++) {
+      albumCollection[i] = new Album(*&artist[i], *&inTitles[i]);
+    }
+
+    // De-refernce pointers to allow for deletion later
+
+    artist = nullptr;
+    return albumCollection;
   }
-  delete [] inTitles;
-  delete [] inArtists;
-  return * albumCollection;
-}
