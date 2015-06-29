2e92700a331e4275197c461ddbeae3676063b428
Rhernandez513 <rhernandez513@gmail.com>
2015-06-28 22:37:01 -0500

    finished lab4

--- lab4/lab4-1.cpp
+++ lab4/lab4-1.cpp
@@ -2,167 +2,184 @@
 
 class Node
 {
-
-    public:
-        int data;
-        Node * link;
+  public:
+    int data;
+    Node * link;
 };
 
 class Llist
 {
-    private:
-        Node *Head, *Tail;
-    public:
-        Llist();
-        void Append(Node * NewNode);
-        void Insert_at_Pos(Node * NewNode, int position);
-        void DeleteNode(int position);
-        void show_nodes();
-        Node * search_for_node(int data);
-        int length_list();
+  private:
+    Node * Head, * Tail;
+  public:
+    Llist();
+    void Append(Node * NewNode);
+    void Insert_at_Pos(Node * NewNode, int position);
+    void DeleteNode(int position);
+    void show_nodes();
+    Node * search_for_node(int data);
+    int length_list();
 };
 
 using namespace std;
 
 Llist::Llist()
 {
-    Head=NULL;
+  this->Head = nullptr;
 }
 
 void Llist::Append(Node * NewNode)
 {
-    if (Head == NULL) {
-        Head=NewNode;
-        Tail=NewNode;
-    } else {
-        Tail->link=NewNode;
-        Tail=NewNode;
-    }
+  if (this->Head  ==  nullptr) {
+    this->Head = NewNode;
+    this->Tail = NewNode;
+  } else {
+      this->Tail->link = NewNode;
+      this->Tail = NewNode;
+  }
 }
 
 void Llist::Insert_at_Pos(Node * NewNode, int position)
 {
-    Node *temp=Head;
-    if (position == 1)     //we will insert at the first position
-    {
-        NewNode->link = temp;
-        Head = NewNode;           //update the head
-    } else {
-        int count=1, flag=1;
-        while(count !=position-1) {
-            temp=temp->link;
-            if(temp==NULL) {flag=0; break;}
-            count++;
+  Node *temp = this->Head;
+  if (position  ==  1) {   //we will insert at the first position
+    NewNode->link = temp;
+    this->Head = NewNode;           //update the head
+  } else {
+     int flag = 1;
+     for (int count = 1; count != (position - 1); count++) {
+       temp = temp->link;
+       if (temp == nullptr) {
+         flag = 0;
+         std::cout << "Position not found.\n";
+         break;
         }
-        if (flag==1) {
-            NewNode->link=temp->link;
-            temp->link=NewNode;
-        }
-        else
-            cout << "Position not found.\n";
-    }
+      }
+      if (flag == 1) {
+        NewNode->link = temp->link;
+        temp->link = NewNode;
+      }
+  }
 }
 
 void Llist::DeleteNode(int position)
 {
-    Node *temp=Head;
-    if(position==1)     //we will delete the first node
-    {
-        Head=temp->link;
-        delete temp;           //delete the node
-    }
-    else
-    {
-        int count=1, flag=1;
-        Node *curr;
-        while(count!=position-1)
-        {
-            temp=temp->link;
-            if(temp==NULL) {flag=0; break;}
-            count++;
+  Node *temp = this->Head;
+  if (position == 1) {   //we will delete the first node
+      this->Head = temp->link;
+      delete temp;           //delete the node
+  } else {
+      int flag = 1;
+      Node *curr;
+     for (int count = 1; count != (position - 1); count++) {
+       temp = temp->link;
+       if (temp == nullptr) {
+         flag = 0;
+         std::cout << "Position not found.\n";
+         break;
         }
-        if(flag==1)
-        {
-            curr=temp->link;
-            temp->link=curr->link;
-            delete curr;
-        }
-        else
-            cout << "Position not found.\n";
-    }
+      }
+      if (flag == 1) {
+        curr = temp->link;
+        temp->link = curr->link;
+        delete curr;
+      }
+  }
 }
 
 void Llist::show_nodes()
 {
-    int i=1;
-    Node * temp;
-    temp=Head;
-    cout << i << "\t" << temp->data << "\n";
+  int i = 1;
+  Node * temp;
+  temp = this->Head;
+  std::cout << i << "\t" << temp->data << "\n";
+  i++;
+  while (temp->link != nullptr) {
+    temp = temp->link;
+    std::cout << i << "\t" << temp->data << "\n";
     i++;
-    while(temp->link!=NULL)
-    {
-        temp=temp->link;
-        cout << i << "\t" << temp->data << "\n";
-        i++;
-    }
+  }
 }
 
 /******************* Week 6, Exercise 1 *******************/
+/// <summary> Finds a Node </summary>
+/// <params> Integer to search for </params>
+/// <returns> Node that matches data member </returns>
 Node * Llist::search_for_node(int data)
 {
-    //write your function here
-    return NULL;		//You'll need to fix this. Return 0 included to compile
+  Node * temp = this->Head;
+  for (int i = 0; temp != nullptr; i++) {
+    if (temp->data == data) { 
+      break;
+    }
+    temp = temp->link;
+  }
+  return temp;
 }
 
+/// <summary> Finds the Length of the Linked List </summary>
+/// <returns> Position of data member </returns>
 int Llist::length_list()
 {
-    //write your function here
-    return 0;		//You'll need to fix this. Return 0 included to compile
+  Node * temp = this->Head;
+  int count = 0;
+  while (temp->link != nullptr) {
+    temp = temp->link;
+    count++;
+  }
+  return count;
 }
 
 /******************* Week 6, Exercise 1 *******************/
 
 int main()
 {
-    Llist x;                //create the linked list
+  Llist x;                //create the linked list
 
-    //create a node to add to list
-    Node *node1=new Node;
-    node1->data=10;
-    node1->link=NULL;
-    x.Append(node1);
+  //create a node to add to list
+  Node *node1 = new Node;
+  node1->data = 10;
+  node1->link = nullptr;
+  x.Append(node1);
 
-    //create a node to add to list
-    Node *node2=new Node;
-    node2->data=5;
-    node2->link=NULL;
-    x.Append(node2);
-    x.show_nodes();
+  //create a node to add to list
+  Node *node2 = new Node;
+  node2->data = 5;
+  node2->link = nullptr;
+  x.Append(node2);
+  x.show_nodes();
 
-    //create a node to add to list at 2nd position
-    Node *node3=new Node;
-    node3->data=1;
-    node3->link=NULL;
-    x.Insert_at_Pos(node3,2);
-    x.show_nodes();
+  //create a node to add to list at 2nd position
+  Node *node3 = new Node;
+  node3->data = 1;
+  node3->link = nullptr;
+  x.Insert_at_Pos(node3,2);
+  x.show_nodes();
 
-    //test the length_list function
-    cout << "The list has " << x.length_list() << " elements." << endl;
-    cout << endl;
+  //test the length_list function
+  std::cout << "The list has " << x.length_list() << " elements." << std::endl;
+  std::cout << std::endl;
 
-    //test search_for_node function
-    Node * temp;
-    temp=x.search_for_node(1);
-    if(temp!=NULL) cout << "I found " << temp->data << endl;
+  //test search_for_node function
+  Node * temp;
+  temp = x.search_for_node(1);
+  if (temp != nullptr) {
+    std::cout << "I found " << temp->data << std::endl;
+  }
 
-    //delete a node and test the length_list function again
-    x.DeleteNode(2);
-    x.show_nodes();
-    cout << "The list has " << x.length_list() << " elements." << endl;
+  //delete a node and test the length_list function again
+  x.DeleteNode(2);
+  x.show_nodes();
+  std::cout << "The list has " << x.length_list() << " elements." << std::endl;
 
-    //test search_for_node function --> Should not find it; it is looking for the deleted node
-    temp=x.search_for_node(1);
-    if(temp!=NULL) cout << "I found " << temp->data << endl;
+  //test search_for_node function --> Should not find it; it is looking for the deleted node
+  temp = x.search_for_node(1);
+  if (temp != nullptr) {
+    std::cout << "I found " << temp->data << std::endl;
+  } else {
+    std::cout << "Looks like I didn't find anything." << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
+
