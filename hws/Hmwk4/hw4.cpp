8e0afbb51beb6fda63379a705468d4d5e8f9b65e
Robert Hernandez <rhernandez513@gmail.com>
2015-07-05 00:01:56 -0500

    did some work on hw4, needs debugging, throwing iterator dereferencing error

--- hws/Hmwk4/hw4.cpp
+++ hws/Hmwk4/hw4.cpp
@@ -2,25 +2,31 @@
 #include <stack>
 #include <exception>
 #include <string>
+#include <fstream>
 #include <cstring>
 #include <algorithm>
 #include <vector>
 
-//using namespace std;
 
-struct conversion{
+struct conversion
+{
   std::string infix;
   std::string postfix;
   std::string prefix;
 };
+bool isOperand(const char C);
+int inStackPriority(const char ch);
+int incomingPriority(const char ch);
+bool hasHigherPrecedenceInToPre(const char op1, const char op2);
+bool writeSolutionFile(std::vector<conversion> & solutions);
 
-std::string infixToPrefix(const std::string expression)
+std::string infixToPrefix(std::string expression)
 {
   std::stack<char> operatorStack, displayStack;
   std::vector<char> reversedInput;
   std::reverse_copy(expression.begin(), expression.end(), reversedInput.begin());
   std::string result;
-  for (int i= reversedInput.begin();*i!= reversedInput.end(); i++) {
+  for (auto i= reversedInput.begin(); i != reversedInput.end(); i++) {
     if (isOperand(*i)) {
       displayStack.push(*i);
     }
@@ -33,7 +39,7 @@ std::string infixToPrefix(const std::string expression)
         operatorStack.pop();
       } 
     }
-    if (isOperator(*i)) {
+    if (!isOperand(*i)) {
       if (incomingPriority(*i) >= inStackPriority(operatorStack.top())) {
         operatorStack.push(*i);
       } else {
@@ -58,16 +64,16 @@ std::string infixToPrefix(const std::string expression)
 }
 
 
-std::string infixToPostfix(const std::string expression) 
+std::string infixToPostfix(std::string expression) 
 {
   std::string result;
   std::stack<char> holdingStack;
-  for (int i= std::begin(expression);*i!= std::end(expression); i++) {
-    if (i == ')') {
-       *i= holdingStack.top();
+  for (auto i= std::begin(expression); i != std::end(expression); i++) {
+    if (*i == ')') {
+       *i = holdingStack.top();
         holdingStack.pop();
-      while (*i!= '(') {
-       *i= holdingStack.top();
+      while (*i != '(') {
+       *i = holdingStack.top();
         std::cout << *i;
         holdingStack.pop();
       }
@@ -75,10 +81,10 @@ std::string infixToPostfix(const std::string expression)
     if (isOperand(*i)) {
       std::cout << *i;
     } else {
-      if (hasHigherPrecedenceInToPre(i, holdingStack.top()) {
+      if (hasHigherPrecedenceInToPre(*i, holdingStack.top())) {
         holdingStack.push(*i);
       } else {
-        while (!hasHigherPrecedenceInToPre(i, holdingStack.top())) {
+        while (!hasHigherPrecedenceInToPre(*i, holdingStack.top())) {
         std::cout << holdingStack.top();
         holdingStack.pop();
         }
@@ -97,7 +103,7 @@ std::string infixToPostfix(const std::string expression)
 conversion* readFile(const std::string filename)
 {
   int count = 0;
-  std::fstream inputStream(&filename, std::fstream::in);
+  std::fstream inputStream(filename.c_str(), std::fstream::in);
   bool openCheck = inputStream.is_open() ? true : false;
   if (openCheck) {
     // Reads the file to the end of the line and determines # of lines
@@ -106,20 +112,24 @@ conversion* readFile(const std::string filename)
       std::getline(inputStream, line);
       count++;
     }
+    std::cout << "Count: " << count << std::endl;
     // Reset back to beginning of file
     inputStream.clear();
     inputStream.seekg(0);
     // Holds all of the data of the file in a string array
     conversion* fileData = new conversion[count];
-    for (int i= 0;*i<= count; i++;) {
+    for (int i = 0; i < count; i++) {
       // Places info from the file into memory and writes to screen
-      std::getline(inputStream, fileData[*i].infix);
-      std::cout << fileData[*i].infix << std::endl;
+      system("pause");
+      std::getline(inputStream, fileData[i].infix);
+      std::cout << fileData[i].infix << std::endl;
     }
+    // Cleanup
+    inputStream.close();
+    return fileData;
+  } else {
+    return nullptr;
   }
-  // Cleanup
-  inputStream.close();
-  return fileData;
 }
 
 bool fileOption(const std::string filename)
@@ -127,7 +137,7 @@ bool fileOption(const std::string filename)
   try
   {
     conversion * fileData = readFile(filename);
-    for (int i= 0; fileData[i] != nullptr; i++) {
+    for (int i= 0; &fileData[i] != nullptr; i++) {
       fileData[i].postfix = infixToPostfix(fileData[i].infix);
       fileData[i].prefix = infixToPrefix(fileData[i].infix);
     }
@@ -194,11 +204,13 @@ bool isOperand(const char C)
   }
 }
 
-//This should return true if isp>icp else false, also should check for right associativity by comapring isp==icp and char is '^'
-bool hasHigherPrecedenceInToPre(const char op1, const char op2)
+//This should return true if isp>icp else false, also should check for right 
+// associativity by comapring isp==icp and char is '^'
+bool hasHigherPrecedenceInToPre(char op1, char op2)
 {
   /* ===== INCOMPLETE ===== */
- incomingPriority(op1) > inStackPriority(op2) ? return true : return false;
+ bool result_b = incomingPriority(op1) > inStackPriority(op2) ? true : false;
+ return result_b;
 }
 
 //This should prompt user to enter 1-3 for a choice or 0 to quit
@@ -215,11 +227,8 @@ void menuOption(int &choice)
     if (choice > 3 || choice < 0) {
       std::cout << "Invalid Input Try Again." << std::endl;
     }
+    // This makes an infinite loop, fix
   } while (choice > 3 || choice < 0);
-  if (choice == 3) {
-    std::cout << "What is the path to the file? ";
-    fileOption(std::cin);
-  }
 }
 
 int main()
@@ -234,7 +243,17 @@ int main()
   std::vector<conversion> solutions;
   int choice;
   menuOption(choice);
-  
+  if (choice == 3) {
+    std::string temp_string;
+    std::cout << "What is the path to the file? ";
+    std::cin >> temp_string;
+    if (fileOption(temp_string)) {
+      std::cout << "File read successfully." << std::endl;
+    } else {
+      std::cout << "File reading failed, try again." << std::endl;
+    }
+  }
+  system("pause");
   return 0;
 }
 
