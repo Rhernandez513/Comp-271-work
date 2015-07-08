1cd6878f4681dacb2e907761fa4edc205cba734b
Robert Hernandez <rhernandez513@gmail.com>
2015-07-08 02:22:52 -0500

    hw4.cpp finally done

--- hws/Hmwk4/hw4.cpp
+++ hws/Hmwk4/hw4.cpp
@@ -3,61 +3,68 @@
 #include <exception>
 #include <string>
 #include <fstream>
-#include <cstring>
 #include <algorithm>
 #include <vector>
 
-
+// Container for equations of data (as strings)
 struct conversion
 {
   std::string infix;
   std::string postfix;
   std::string prefix;
 };
+
+std::vector<conversion> file_Data;
 bool isOperand(const char C);
 int inStackPriority(const char ch);
 int incomingPriority(const char ch);
-bool hasHigherPrecedenceInToPre(const char op1, const char op2);
+bool hasHigherPrecedenceInFixToPre(const char op1, const char op2);
 bool writeSolutionFile(std::vector<conversion> & solutions);
 bool isOperator(const char C);
+bool hasHigherPrecedenceInToPost(char op1, char op2);
+void writeSolutionsFile(std::vector<conversion> solutions);
 int count = 0;
+
+// Business Logic for converting Infix to Postfix
 std::string infixToPrefix(std::string expression)
 {
   std::stack<char> operatorStack, displayStack;
   std::vector<char> reversedInput(256);
+  std::string result;
+  int leftParensCount = 0, rightParensCount = 0;
+  
   std::reverse_copy(expression.begin(), expression.end(), reversedInput.begin());
   reversedInput.shrink_to_fit();
-  std::string result;
-  for (auto i= reversedInput.begin(); i != reversedInput.end(); i++) {
+  
+  for (auto i= reversedInput.begin(); *i != '\0'; ++i) {
+    // Tracks Parenthesis
+    if (*i == ')') {
+      ++rightParensCount;
+    } else if (*i == '(') {
+      ++leftParensCount;
+    }
+
     if (isOperand(*i)) {
       displayStack.push(*i);
     }
-    if (*i == ')' && !operatorStack.empty()) {
-        *i = operatorStack.top();
-        operatorStack.pop();
-      while (*i != '(') {
+    if (*i == ')') {
+      while (!operatorStack.empty() && *i != '(') {
         displayStack.push(*i);
-        if (operatorStack.empty()) {
-          break;
-        }
         *i = operatorStack.top();
         operatorStack.pop();
       } 
     }
-    if (!isOperand(*i)) {
+    if (isOperator(*i)) {
       if (operatorStack.empty()) {
         operatorStack.push(*i);
-      } else if (hasHigherPrecedenceInToPre(*i, operatorStack.top())) {
+      } else if (hasHigherPrecedenceInFixToPre(*i, operatorStack.top())) {
         operatorStack.push(*i);
       } else {
-        if (!operatorStack.empty()) {
-          while (!hasHigherPrecedenceInToPre(*i, operatorStack.top())) {
-            displayStack.push(operatorStack.top());
-            operatorStack.pop();
-            if (operatorStack.empty()) {
-              break;
-            }
-          }
+        while (!operatorStack.empty() &&
+               !hasHigherPrecedenceInFixToPre(*i, operatorStack.top())
+              ) {
+          displayStack.push(operatorStack.top());
+          operatorStack.pop();
         }
         operatorStack.push(*i);
       }
@@ -68,26 +75,36 @@ std::string infixToPrefix(std::string expression)
     operatorStack.pop();
   }
   while (!displayStack.empty()) {
-    if (displayStack.top() != '\0') {
-      result += displayStack.top();
-    }
+    char ch = displayStack.top();
     displayStack.pop();
+    if (ch != ')' && ch != '(') {
+      result += ch;
+    }
   }
-  std::cout << result << std::endl;
+  if (leftParensCount != rightParensCount) {
+    result += "    <-- Parenthesis not matched in input, may not be correct.";
+  }
   return result;
 }
 
-
+// Business Logic for converting Infix to Postfix
 std::string infixToPostfix(std::string expression) 
 {
   std::string result;
   std::stack<char> holdingStack;
+  int leftParensCount = 0, rightParensCount = 0;
+
   for (auto i = std::begin(expression); i != std::end(expression); ++i) {
+    if (*i == ')') {
+      ++rightParensCount;
+    } else if (*i == '(') {
+      ++leftParensCount;
+    }
     if (*i == ')') {
        *i = holdingStack.top();
         holdingStack.pop();
       while (*i != '(') {
-        if (!holdingStack.empty()) {
+        if (holdingStack.empty()) {
           break;
         } else {
           *i = holdingStack.top();
@@ -97,41 +114,36 @@ std::string infixToPostfix(std::string expression)
     }
     if (isOperand(*i)) {
       result += *i;
-      std::cout << *i;
     }
     if (isOperator(*i)) {
-      if (holdingStack.empty()) {
-        holdingStack.push(*i);
-        continue;
-      }
-      if (hasHigherPrecedenceInToPre(*i, holdingStack.top())) {
-        holdingStack.push(*i);
-      } else {
-        if (!holdingStack.empty()) {
-          while (!hasHigherPrecedenceInToPre(*i, holdingStack.top())) {
-            std::cout << holdingStack.top();
+      if (!holdingStack.empty()) {
+        if (hasHigherPrecedenceInToPost(*i, holdingStack.top())) {
+          holdingStack.push(*i);
+        } else {
+          while (!holdingStack.empty() &&
+                 !hasHigherPrecedenceInToPost(*i, holdingStack.top())
+                ) {
             result += holdingStack.top();
             holdingStack.pop();
-            if (holdingStack.empty()) {
-              break;
-            }
           }
           holdingStack.push(*i);
         }
+      } else {
+        holdingStack.push(*i);
       }
     }
   }
   while (!holdingStack.empty()) {
-    std::cout << holdingStack.top();
     result += holdingStack.top();
     holdingStack.pop();
   }
-  std::cout << std::endl;
+  if (leftParensCount != rightParensCount) {
+    result += "    <-- Parenthesis not matched in input, may not be correct.";
+  }
   return result;
 }
-
-
-conversion* readFile(const std::string filename)
+// Performs file reading & populates the conversion vector with Infix Data
+bool readFile(const std::string filename, std::vector<conversion> &file_Data)
 {
   std::fstream inputStream(filename.c_str(), std::fstream::in);
   bool openCheck = inputStream.is_open() ? true : false;
@@ -147,40 +159,46 @@ conversion* readFile(const std::string filename)
     inputStream.clear();
     inputStream.seekg(0);
     // Holds all of the data of the file in a string array
-    conversion* fileData = new conversion[count];
+    std::cout << "Contents of the file being read: \n" << std::endl;
     for (int i = 0; i < count; i++) {
       // Places info from the file into memory and writes to screen
-      std::getline(inputStream, fileData[i].infix);
-      std::cout << fileData[i].infix << std::endl;
+      conversion temp_conversion;
+      std::getline(inputStream, temp_conversion.infix);
+      file_Data.push_back(temp_conversion);
+      std::cout << file_Data[i].infix << std::endl;
     }
     // Cleanup
     inputStream.close();
-    return fileData;
+    return true;
   }
-  return nullptr;
+  return false;
 }
-
+// Calls conversion functions & populates the file_Data struct with 
+// processed data
 bool fileOption(const std::string filename)
 {
   try
   {
-    conversion * fileData = readFile(filename);
-    for (int i= 0; i < count; i++) {
-      fileData[i].postfix = infixToPostfix(fileData[i].infix);
-      fileData[i].prefix = infixToPrefix(fileData[i].infix);
+    if (readFile(filename, file_Data)) {
+      for (int i = 0; i < file_Data.size(); i++) {
+        file_Data[i].postfix = infixToPostfix(file_Data[i].infix);
+        file_Data[i].prefix = infixToPrefix(file_Data[i].infix);
+      }
+      return true;
+    } else {
+      return false;
     }
-    return true;
   }
   catch (std::exception &e)
   {
     std::cout << "Standard Exception: " << e.what() << "\n";
+    return true;
   }
-  return false;
 }
 
 bool writeSolutionFile(std::vector<conversion> & solutions);
 
-//Returns the number for instack priority
+//Returns the number for InStack Priority
 int inStackPriority(const char ch)
 {
   switch (ch) {
@@ -194,12 +212,13 @@ int inStackPriority(const char ch)
       return 1;
     case('('):
       return 0;
-    case('\n'):
+    default:
       return -2;
+
   }
 }
 
-//Returns the number for incoming priority
+//Returns the number for InComing Priority
 int incomingPriority(const char ch)
 {
   switch (ch) {
@@ -212,10 +231,12 @@ int incomingPriority(const char ch)
     case('+'):
     case('-'):
       return 1;
+    default:
+      return -2;
   }
 }
 
-//Should check char to see if it is letter or number (caps,lowercase, and number)
+// Returns True is Char is Operand, false otherwise
 bool isOperand(const char C)
 {
   switch(C) {
@@ -231,7 +252,7 @@ bool isOperand(const char C)
       return true;
   }
 }
-
+// Returns True is Char is Operator, false otherwise
 bool isOperator(const char C)
 {
   switch (C) {
@@ -246,17 +267,29 @@ bool isOperator(const char C)
       return false;
   }
 }
-
-//This should return true if isp>icp else false, also should check for right 
-// associativity by comapring isp==icp and char is '^'
-bool hasHigherPrecedenceInToPre(char op1, char op2)
+// Infix-to-Prefix Priority Checker
+bool hasHigherPrecedenceInFixToPre(char op1, char op2)
 {
-  /* ===== INCOMPLETE ===== */
   if (incomingPriority(op1) >= inStackPriority(op2)) {
     return true;
   } else {
     return false;
   }
+}
+// Infix-to-Postfix Priority Checker
+bool hasHigherPrecedenceInToPost(char op1, char op2)
+{
+  if (incomingPriority(op1) > inStackPriority(op2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Helps with clearing std::cin buffer to prevent infinite loops
+void bufferClear()
+{
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 //This should prompt user to enter 1-3 for a choice or 0 to quit
@@ -268,36 +301,132 @@ void menuOption(int &choice)
     std::cout << "2. Infix to Postfix Conversion\n" << std::endl;
     std::cout << "3. Read in a file of Infix Equations\n" << std::endl;
     std::cout << "0 to quit\n" << std::endl;
-    std::cout << "Choice: ";
+    std::cout << "Choice:  ";
     std::cin  >>  choice;
-    if (choice > 3 || choice < 0) {
-      std::cout << "Invalid Input Try Again." << std::endl;
+    if (choice == 0) {
+      return;
+    } else if (choice > 3 || choice < 0) {
+      std::cout << "Invalid Input Try Again.\n\n";
+      bufferClear();
     }
-    // This makes an infinite loop, fix
   } while (choice > 3 || choice < 0);
 }
-
+// Provides User prompt and Error Checking of User input for writing
+void promptUsertoWriteFile()
+{
+  std::string userReponse;
+  std::cout << "Write to text File? (y/n)" << std::endl;
+  std::cin >> userReponse;
+  if (userReponse == "Y" || userReponse == "y") {
+    writeSolutionsFile(file_Data);
+  } else if (userReponse == "N" || userReponse == "n") {
+      std::cout << "Goodbye.\n" << std::endl;
+  } else {
+      std::cout << "Invalid Input Try Again.\n\n";
+      bufferClear();
+      promptUsertoWriteFile();
+  }
+}
+// Allows User to view conversion results from the read-in file
+void viewSolutions(std::vector<conversion> solutions)
+{
+  for (int i = 0; i < solutions.size(); i++) {
+    std::cout << "Problem #" << i + 1 << " infix: ";
+    std::cout<< solutions[i].infix << std::endl;
+    std::cout<< "Problem #" << i + 1 << " postfix: ";
+    std::cout<< solutions[i].postfix << std::endl;
+    std::cout<< "Problem #" << i + 1 << " prefix: ";
+    std::cout<< solutions[i].prefix << std::endl;
+    std::cout<< "\n" << std::endl;
+  }
+}
+// Allows User to view conversion results from the read-in file
+void promptUsertoViewResults()
+{
+  std::string userReponse;
+  std::cout << "View Solutions? (y/n)" << std::endl;
+  std::cin >> userReponse;
+  if (userReponse == "Y" || userReponse == "y") {
+    viewSolutions(file_Data);
+    bufferClear();
+  }
+}
+// Provides User prompt and Error Checking of User input for reading
+// Recursive
+void promptUserForFileName(std::string temp_string)
+{
+  bufferClear();
+  std::cout << "What is the path to the file?  ";
+  std::cin >> temp_string;
+  if (fileOption(temp_string)) {
+    std::cout << "\nFile read successfully.\n\n" << std::endl;
+    promptUsertoViewResults();
+    promptUsertoWriteFile();
+  } else if (temp_string.c_str() == "0") {
+    std::cout << "Goodbye.\n" << std::endl;
+  } else {
+    std::cout << "File reading failed, try again.\n\n" << std::endl;
+    promptUserForFileName(temp_string);
+  }
+}
+// User-Interactive Prefix Conversion
+void infixToPrefixOption()
+{
+  bufferClear();
+  std::string result;
+  std::cout << "\n\nPlease Enter the Equation you would like to convert"
+            << std::endl;
+  std::getline(std::cin, result);
+  std::cout << "\nResult: " << infixToPrefix(result) << std::endl;
+  std::system("pause");
+}
+// User-Interactive Postfix Conversion
+void infixToPostfixOption()
+{
+  bufferClear();
+  std::string result;
+  std::cout << "\n\nPlease Enter the Equation you would like to convert"
+    << std::endl;
+  std::getline(std::cin, result);
+  std::cout << "\nResult: " << infixToPostfix(result) << std::endl;
+  std::system("pause");
+}
+// Writes Final solutions to a text file
+void writeSolutionsFile(std::vector<conversion> solutions)
+{
+  std::string fileName = "solutions.txt";
+  std::fstream solution_fstream(fileName, std::fstream::out);
+  if (solution_fstream.is_open())
+  {
+    for (int i = 0; i < solutions.size(); i++) {
+      solution_fstream << "Problem #" << i + 1 << " infix: ";
+      solution_fstream << solutions[i].infix << std::endl;
+      solution_fstream << "Problem #" << i + 1 << " postfix: ";
+      solution_fstream << solutions[i].postfix << std::endl;
+      solution_fstream << "Problem #" << i + 1 << " prefix: ";
+      solution_fstream << solutions[i].prefix << std::endl;
+      solution_fstream << "\n" << std::endl;
+    }
+    solution_fstream.close();
+  } else {
+    std::cout << "Failed to write file." << std::endl;
+  }
+}
 int main()
 {
-  /*********
-   *Variables you will need: a string for the expression the user will input,
-   *string for the output you get from each function, a int for choice of what the user enters. 
-   *If a user enters a choice that is NOT part of the menu prompt Not a valid choice try again.
-   *A vector that holds conversion data type called solutions
-  **********/
   std::string* userInput = new std::string;
   std::vector<conversion> solutions;
-  int choice;
+  int choice = -1;
   menuOption(choice);
-  if (choice == 3) {
+  if (choice == 0) {
+    std::cout << "Goodbye.\n" << std::endl;
+  } else if (choice == 1) {
+      infixToPrefixOption();
+  } else if (choice == 2) {
+      infixToPostfixOption();
+    } else if (choice == 3) {
     std::string temp_string;
-    std::cout << "What is the path to the file? ";
-    std::cin >> temp_string;
-    if (fileOption(temp_string)) {
-      std::cout << "File read successfully." << std::endl;
-    } else {
-      std::cout << "File reading failed, try again." << std::endl;
-    }
+    promptUserForFileName(temp_string);
   }
   std::system("pause");
   return 0;
