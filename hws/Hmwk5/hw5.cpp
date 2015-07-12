5c1e2216bfc544aae631a427cd5b2884aed7db22
Robert Hernandez <rhernandez513@gmail.com>
2015-07-12 03:16:03 -0500

    lab5 done, minor edits to hw5

--- hws/Hmwk5/hw5.cpp
+++ hws/Hmwk5/hw5.cpp
@@ -14,52 +14,53 @@
 
 using namespace std;
 
-struct word_freq{
-    string word;
-    int num_times_occ;
+struct word_freq {
+  std::string word;
+  int num_times_occ;
 };
 
-//this function reads the file and puts it in the vector
-bool read_file(char * file_name,vector<string> & w);
+//this function reads the file and puts it in the std::vector
+bool read_file(char * file_name,std::vector<string> & w);
 
 //this function finds if the word is already in the array and returns the index
-int is_in(string w, vector<word_freq> & wf);
+int is_in(string w, std::vector<word_freq> & wf);
 
 //this function finds how many times a word occurs
-int get_num_occ(string w, vector<word_freq> & wf);
+int get_num_occ(string w, std::vector<word_freq> & wf);
 
 //this function will write a file called 'word_stats.txt'
 //and have all the UNIQUE words and how many times they show up
-bool write_file(char * file_name,vector<word_freq> & wf);
+bool write_file(char * file_name,std::vector<word_freq> & wf);
 
 //this function will check each line and get each word, getting rid of punctuation, newline,tabs, and carriage returns
-bool parse_line(char * word, vector<string> &w);
+bool parse_line(char * word, std::vector<string> &w);
 
-int main() {
+int main()
+{
+  int i;
+  int index;
+  string word;
+  std::vector<string> words;
+  std::vector<word_freq> words_occ;
+  word_freq new_word;
+  string filename;
+  char * c_filename;
+  std::cout << "Please enter a filename: ";
+  std::getline(cin,filename);
+  c_filename = new char[stdstrlen(filename.c_str()) + 1];
+  strcpy(c_filename, filename.c_str());
+  //if file has been read and all the words are in the "words" std::vector 
+  //for loop to go through std::vector words, check is word with is_in function and it should return an index
+  //if index is greater than or equal to 0 the words_occ[index].num_times_occ increased
+  //else new word set the word and occurences to 1 and pushback into '"words_occ"
 
-    int i;
-    int index;
-    string word;
-    vector<string> words;
-    vector<word_freq> words_occ;
-    word_freq new_word;
-    string filename;
-    char * c_filename;
-    cout << "Please enter a filename: ";
-    getline(cin,filename);
-    c_filename=new char[strlen(filename.c_str())+1];
-    strcpy(c_filename, filename.c_str());
-    //if file has been read and all the words are in the "words" vector 
-    //for loop to go through vector words, check is word with is_in function and it should return an index
-    //if index is greater than or equal to 0 the words_occ[index].num_times_occ increased
-    //else new word set the word and occurences to 1 and pushback into '"words_occ"
-   
-    //Show each word and how many times it occurs example: for loop of 'words_occ'
-    //Write the file of all the occurences
-  
-    //while loop to ask user "What word do you want to look up or 0 to quit"
-    //if word is there say word and how many times it occured
-    //else word not in vector or 0 to quit
+  //Show each word and how many times it occurs example: for loop of 'words_occ'
+  //Write the file of all the occurences
 
-    return 1;
+  //while loop to ask user "What word do you want to look up or 0 to quit"
+  //if word is there say word and how many times it occured
+  //else word not in std::vector or 0 to quit
+
+  return 1;
 }
+
--- labs/Lab5/lab5.cpp
+++ labs/Lab5/lab5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class TreeNode
 {
-public:
+  public:
     char Data;
     TreeNode *Lchild;
     TreeNode *Rchild;
@@ -13,10 +13,10 @@ public:
 
 class BinaryTree
 {
-public:
+  public:
     TreeNode *Root;
-public:
-    BinaryTree() {Root=NULL;}       //constructor
+  public:
+    BinaryTree() { Root = nullptr; }       //constructor
     void Preorder();
     void Inorder();
     void Postorder();
@@ -30,163 +30,215 @@ public:
 // 1. finish up the preorder, look at slides
 void BinaryTree::Preorder()
 {
-    TreeNode * Tmp=Root;
-    stack<TreeNode*> S;
-    while(1)
-    {
-	
-      
+  TreeNode * Tmp = Root;
+  stack<TreeNode*> S;
+  while (true) {
+    while (Tmp != nullptr) {
+      std::cout << Tmp->Data;
+      S.push(Tmp);
+      Tmp = Tmp->Lchild;
     }
+    if (S.empty()) {
+      return;
+    }
+    Tmp = S.top();
+    S.pop();
+    Tmp = Tmp->Rchild;
+  }
 }
 
 void BinaryTree::Inorder()
 {
-    TreeNode * Tmp=Root;
-    stack<TreeNode*> S;
-    while(1)
-    {
-        //traverse left until its NULL & push
-        while(Tmp!=NULL)
-        {
-            S.push(Tmp);
-            Tmp=Tmp->Lchild;
-        }
-        //if stack is empty stop the process
-        if(S.empty()) return;
-        
-        //if not empty, pop 1 and go right
-        Tmp=S.top();
-        S.pop();
-        cout << Tmp->Data;
-        Tmp=Tmp->Rchild;
+  TreeNode * Tmp = Root;
+  stack<TreeNode*> S;
+  while (true) {
+    //traverse left until its nullptr & push
+    while (Tmp != nullptr) {
+      S.push(Tmp);
+      Tmp = Tmp->Lchild;
     }
+    //if stack is empty stop the process
+    if (S.empty()) {
+      return;
+    }
+    //if not empty, pop 1 and go right
+    Tmp = S.top();
+    S.pop();
+    std::cout << Tmp->Data;
+    Tmp = Tmp->Rchild;
+  }
 }
 
 void BinaryTree::Postorder()
 {
-    TreeNode * Tmp=Root;
-    stack<TreeNode*> S;		//stores nodes
-    stack<char> F;	//stores flag
-    char flag;	//íLí or ëRí
-    while(1)
-    {
-        //traverse left until its NULL & push
-        while(Tmp!=NULL)
-        {
-            S.push(Tmp);
-            F.push('L');
-            Tmp=Tmp->Lchild;
-        }
-        //if stack is empty stop the process
-        if(S.empty()) return;
-        else
-        {
-            Tmp=S.top();
-            S.pop();		//pop node
-            flag=F.top();
-            F.pop();		//pop flag value
-            if(flag=='R')
-            {
-                cout << Tmp->Data;
-                Tmp=NULL;
-            }
-            else	//if flag is ëLí
-            {
-                S.push(Tmp);
-                F.push('R');	//push Tmp with ëRí
-                Tmp=Tmp->Rchild;	//move to right
-            }
-        }
+  TreeNode * Tmp = Root;
+  stack<TreeNode*> S;
+  //stores nodes
+  stack<char> F;
+  //stores flag
+  char flag;
+  //'L' or 'R'
+  while (1) {
+    //traverse left until its nullptr & push
+    while (Tmp != nullptr) {
+      S.push(Tmp);
+      F.push('L');
+      Tmp = Tmp->Lchild;
     }
+    //if stack is empty stop the process
+    if (S.empty()) { 
+      return;
+    } else {
+      Tmp = S.top();
+      S.pop();
+      //pop node
+      flag = F.top();
+      F.pop();
+      //pop flag value
+      if (flag == 'R') {
+        std::cout << Tmp->Data;
+        Tmp = nullptr;
+      } else {  //if flag is 'L'
+        S.push(Tmp);
+        //push Tmp with 'R'
+        F.push('R');
+        //move to right
+        Tmp = Tmp->Rchild;
+      }
+    }
+  }
 }
 
 //#2 Recursive version of PREORDER
 void BinaryTree::PreorderR(TreeNode*)
 {
-	//write recursive version of preorder
+  if (Root != nullptr) {
+    std::cout << Root->Data; /* D         D      */
+    PreorderR(Root->Lchild); /* L        / \     */
+    PreorderR(Root->Rchild); /* R       L   R    */
+  }
 }
 
 //#3 Recursive version of INORDER
 void BinaryTree::InorderR(TreeNode*)
 {
-   //write recursive version of inorder
+  if (Root != nullptr) {
+    InorderR(Root->Lchild);  /* L         D      */
+    std::cout << Root->Data; /* D        / \     */
+    InorderR(Root->Rchild);  /* R       L   R    */
+  }
 }
 
 //#4 Recursive version of POSTORDER
 void BinaryTree::PostorderR(TreeNode*)
 {
-  //write recursive version  of postorderf
+  if (Root != nullptr) {
+    PreorderR(Root->Lchild); /* L         D      */
+    PreorderR(Root->Rchild); /* R        / \     */
+    std::cout << Root->Data; /* D       L   R    */
+  }
 }
 
 //#5 in the INORDER fashion look for node where Temp->Data==D and return node
 TreeNode* BinaryTree::Find_Node_Inorder(char D)
 {
-    TreeNode * Tmp=Root;
-    stack<TreeNode*> S;
-    while(1)
-    {
-       	//traverse left until its NULL & push
-       	//if not empty, pop 1 and go right
-      	//OLD code: cout << Tmp->Data; should do a comparison instead
-    
+  TreeNode * Tmp = Root;
+  stack<TreeNode*> S;
+  while (true) {
+    // Go Left until NULL & push
+    while (Tmp != nullptr) {
+      S.push(Tmp);
+      Tmp = Tmp->Lchild;
     }
+    // If Stack is empty, back out
+    if(S.empty()) {
+      return nullptr;
+    }
+    // Check Data
+    Tmp = S.top();
+    S.pop();
+    if (Tmp->Data == D) {
+      return Tmp;
+    }
+    // Go right & start going left again
+    Tmp = Tmp->Rchild;
+  }
 }
 
 //#6 Write the insertNode function
 void BinaryTree::InsertNode(char Val)
 {
-    TreeNode *newNode,	// Pointer to a new node
-    *nodePtr;	// Pointer to traverse the tree
-    
-    // Create a new node called newNode
-    //make that nodes Data variable equal to VAL
-    //set newNode Lchild and Rchild = NULL
-    
-    if (!Root)	// Is the tree empty?
-        Root = newNode;
-    else
-    {
-        nodePtr = Root;
-        while (nodePtr != NULL)
-        {
-            if () //check for < than
-            {
-               //break;
-            }
-            else if () //check for > than
-            {
-				br//eak;
-            }
-            else
-            {
-                cout << "Duplicate value found in tree.\n";
-                break;
-            }
+  // Pointer to a new node
+  // Pointer to traverse the tree
+  TreeNode * newNode, * nodePtr;
+  // Create a new node
+  newNode = new TreeNode;
+  newNode->Data = Val;
+  newNode->Lchild = newNode->Rchild = nullptr;
+  if (!Root) {
+    // Is the tree empty?
+    Root = newNode;
+  } else {
+    nodePtr = Root;
+    while (nodePtr != nullptr) {
+      if (Val < nodePtr->Data) {
+        if (nodePtr->Lchild) {
+          nodePtr = nodePtr->Lchild;
+        } else {
+          nodePtr->Lchild = newNode;
+          break;
         }
+      } else if (Val > nodePtr->Data) {
+        if (nodePtr->Rchild) {
+          nodePtr = nodePtr->Rchild;
+        } else {
+          nodePtr->Rchild = newNode;
+          break;
+        }
+      } else {
+        std::cout << "Duplicate value found in tree.\n";
+        break;
+      }
     }
+  }
 }
-
-
 
 /************************************* MAIN FUNCTION *************************************/
 int main()
 {
-    //#7 Createa a binary tree call b and isnsert nodes m,a,q
-    //Create a binary tree called 'b'
-    //insertNodes 'm','a','q' in this specific order
+  //#7 Create a binary tree call b and insert nodes m,a,q
+  //Create a binary tree called 'b'
+  BinaryTree * b = new BinaryTree();
+  char m = 'm', a = 'a', q = 'q';
+  //insertNodes 'm','a','q' in this specific order
+  b->InsertNode(m);
+  b->InsertNode(a);
+  b->InsertNode(q);
+  TreeNode * nodePtr = new TreeNode;
+  //#8 call recurisve and non recursive functions of order types
+  //call both postorderR and postorder
+  std::cout << "Postorder: ";
+  b->Postorder();
+  std::cout << std::endl;
+  std::cout << "PostorderR: ";
+  //b->PostorderR(nodePtr);
+  std::cout << std::endl;
 
-    //#8 call recurisve and non recursive functions of order types
-    cout << "Postorder: ";
-    //call both postorderR and postorder
-    cout << endl;
-    
-    cout << "Inorder: ";
-     //call both inorderR and inorder
-    cout << endl;
-    
-    cout << "Preorder: ";
-    //call both preorderR and preorder
-    cout << endl;
-    
-    return 0;
+  //call both inorderR and inorder
+  std::cout << "Inorder: ";
+  b->Inorder();
+  std::cout << std::endl;
+  std::cout << "InorderR: ";
+  //b->InorderR(nodePtr);
+  std::cout << std::endl;
+
+  //call both preorderR and preorder
+  std::cout << "Preorder: ";
+  b->Preorder();
+  std::cout << std::endl;
+  std::cout << "PreorderR: ";
+  //b->PreorderR(nodePtr);
+  std::cout << std::endl;
+  std::system("pause");
+  return 1;
 }
