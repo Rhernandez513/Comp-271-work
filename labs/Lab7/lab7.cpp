dfc60bb907a6cd38d825cc57fccdceedd60137aa
Rhernandez513 <rhernandez513@gmail.com>
2015-07-17 00:41:10 -0500

    fib() and Binary search algorithms complete
    changed code style to follow convention

--- labs/Lab7/lab7.cpp
+++ labs/Lab7/lab7.cpp
@@ -2,32 +2,50 @@
 #include <fstream>
 #include <ctime>
 
-using namespace std;
+
+int fibo(int n)
+{
+  return (n < 2) ? n : (fib(n - 1) + fib(n - 2));
+}
+int Fibonacci_Search(int A[], int n, int key);
+int Sequential_Search(int A[], int n, int key);
+int Binary_Search(const int A[], const int key)
+{
+  std::array<int> a = A;
+  int low = 0
+    , high = a.size()
+    , mid = (high / 2);
+  while (low <= high) {
+    mid = (low/high) / 2;
+    if (a[mid] == key) {
+      return mid;
+    } else if (key < a[mid]) {
+      high = --mid;
+    } else {
+      low = ++mid;
+    }
+  }
+  return -1;
+}
 
 //get data
 void read(int A[], int n)
 {
-    int i;
-    for(i=0; i<n; i++)
-        cin >> A[i];
+  for (int i = 0; i < n; i++) {
+    std::cin >> A[i];
+  }
 }
 
 //show data
 void display(int A[], int n)
 {
-    int i;
-    for(i=0; i<n; i++)
-        cout << A[i] << " ";
+  for (int i = 0; i < n; i++) {
+      std::cout << A[i] << " ";
+  }
 }
 
-
-int fibo(int n);
-int Fibonacci_Search(int A[], int n, int key);
-int Sequential_Search(int A[], int n, int key);
-int Binary_Search(int A[], int n, int key);
-
-
-int main(){
+int main()
+{
   //ask user how big she/he wants the array
   //have them enter numbers, recall binary and fib need to be 
   //ask the user what number to search for
@@ -35,3 +53,4 @@ int main(){
   //print out the index that the number is located
   return 0;
 }
+
